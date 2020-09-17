4fd1a67e894aaecee9b1e7c189c71c15dc65611f
Cindy Lau <cindy.lau@shopify.com>
2020-09-17 15:47:29 -0400

    module 8 - binary trees

--- CS2/Assignments/Module 8/Binary Trees - Structure Comparison and Node Counting/BinaryTree.java
+++ CS2/Assignments/Module 8/Binary Trees - Structure Comparison and Node Counting/BinaryTree.java
@@ -33,7 +33,72 @@ public class BinaryTree
     public void setData(String d) { data = d; }
     public void setLeftChild(BinaryTree left) { leftChild = left; }
     public void setRightChild(BinaryTree right) { rightChild = right; }
+
+    /**
+     * Check that the current instance of the binary tree is the same as a specific tree
+     * It is the same if they have exactly the same structure, data at each node, and left and right children
+     * @param tree - the binary tree to compare the current instance with
+     * @return - true if the trees are exactly the same, false otherwise
+     */
+    public boolean hasSameContentsAs(BinaryTree tree) {
+      // check whether this binary tree, and the tree to compare this instance with, has children nodes
+      boolean thisHasAtLeastOneChildNode = (this.leftChild != null) || (this.rightChild != null);
+      boolean otherHasAtLeastOneChildNode = (tree.leftChild != null) || (tree.rightChild != null);
+
+      // base case: check that there are no children nodes, and that the data at the current node is the same
+      if ((this.data == tree.data) && !thisHasAtLeastOneChildNode && !otherHasAtLeastOneChildNode) {
+        return true;
+      }
+
+      // recursive case: check whether the current node has a left and/or right child node(s)
+      boolean thisHasLeftChildNodeOnly = (this.leftChild != null) && (this.rightChild == null);
+      boolean thisHasRightChildNodeOnly = (this.leftChild == null) && (this.rightChild != null);
+      boolean thisHasBothChildNodes = (this.leftChild != null) && (this.rightChild != null);
+
+      boolean otherHasLeftChildNodeOnly = (tree.leftChild != null) && (tree.rightChild == null);
+      boolean otherHasRightChildNodeOnly = (tree.leftChild == null) && (tree.rightChild != null);
+      boolean otherHasBothChildNodes = (tree.leftChild != null) && (tree.rightChild != null);
+
+      // check that the contents of the child node(s) are the same in both trees
+      if (thisHasLeftChildNodeOnly && otherHasLeftChildNodeOnly) {
+        return this.leftChild.hasSameContentsAs(tree.leftChild);
+      } else if (thisHasRightChildNodeOnly && otherHasRightChildNodeOnly) {
+        return this.rightChild.hasSameContentsAs(tree.rightChild);
+      } else if (thisHasBothChildNodes && otherHasBothChildNodes) {
+        return this.leftChild.hasSameContentsAs(tree.leftChild) && this.rightChild.hasSameContentsAs(tree.rightChild);
+      }
+
+      return false;
+    }
     
+    /**
+     * Get the total number of nodes in the binary tree, and return the number
+     * @return - the number of nodes in the tree
+     */
+    public int numberOfNodes() {
+      // tree is empty: no nodes
+      if (this.data == null) {
+        return 0;
+      }
+
+      // base case: no children nodes
+      if ((this.leftChild == null) && (this.rightChild == null)) {
+        return 1;
+      }
+
+      // recursive case: check number of nodes on left side and right side
+      int nodes = 1;
+
+      if (this.leftChild != null) {
+        nodes += this.leftChild.numberOfNodes();
+      }
+
+      if (this.rightChild != null) {
+        nodes += this.rightChild.numberOfNodes();
+      }
+
+      return nodes;
+    }
     
     // Return the height of the tree
     public int height()  
