d02b2f9dfbfddccdca3bbb407b2d96615daf2ec5
Cindy Lau <cindy.lau@shopify.com>
2020-10-08 16:33:16 -0400

    track how many guesses player made

--- ruby-build-a-game/game.rb
+++ ruby-build-a-game/game.rb
@@ -12,3 +12,8 @@ puts "Welcome, #{name}!"
 puts "I've got a random number between 1 and 100."
 puts "Can you guess it?"
 target = rand(100) + 1
+
+# Track how many guesses the player has made
+num_guesses = 0
+
+puts "You've got #{10 - num_guesses} guesses left."
