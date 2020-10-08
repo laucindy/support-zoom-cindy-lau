1b16efb729fbe738cf16a9abfb7467b18c04a54d
Cindy Lau <cindy.lau@shopify.com>
2020-10-08 15:51:05 -0400

    generate a random number between 1 and 100

--- ruby-build-a-game/game.rb
+++ ruby-build-a-game/game.rb
@@ -7,3 +7,8 @@ print "What's your name? "
 name = gets.chomp
 
 puts "Welcome, #{name}!"
+
+# Store a random number for the player to guess
+puts "I've got a random number between 1 and 100."
+puts "Can you guess it?"
+target = rand(100) + 1
