219264118040c693f27b25d1d63ef83313ee7229
Cindy Lau <cindy.lau@shopify.com>
2020-10-13 11:24:42 -0400

    remove line break  after entering name

--- Ruby/Week 2/Happy Birthday to You/part2.rb
+++ Ruby/Week 2/Happy Birthday to You/part2.rb
@@ -4,15 +4,17 @@ name = gets.chomp
 line_number = 0
 
 while line_number < 4
-  print "\nHappy birthday "
+  print "Happy birthday "
 
   if (line_number == 2)
     print "dear #{name}"
+  elsif (line_number == 3)
+    print "to you."
   else
-    print "to you"
+     print "to you"
   end
+
+  print "\n"
 
   line_number += 1
 end
-
-print ".\n"
