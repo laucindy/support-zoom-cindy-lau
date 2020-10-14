e97225ebd056aa31312d0d3d2cdfdf6686523177
Cindy Lau <cindy.lau@shopify.com>
2020-10-13 21:43:18 -0400

    remove unnecessary loop variable

--- Ruby/Week 2/ASCII Art/part2.rb
+++ Ruby/Week 2/ASCII Art/part2.rb
@@ -1,11 +1,11 @@
-print 'Enter a number: '
+print "Enter a number: "
 number = gets.chomp.to_i
 
 row = 1
 
 while row <= number
-  (number - row).times { |i| print ' ' }
-  row.times { |i| print '*' }
+  (number - row).times { print " " }
+  row.times { print "*" }
   print "\n"
   row += 1
 end
