cf15ffa70a1483ddf6248d26ada7ee93ec981fb5
Cindy Lau <cindy.lau@shopify.com>
2020-10-19 10:42:07 -0400

    the wrong way of eliminating repetition, page 165

--- ruby-code-alongs/prices/prices.rb
+++ ruby-code-alongs/prices/prices.rb
@@ -38,3 +38,34 @@ def show_discounts(prices)
 end
 
 show_discounts(prices)
+
+# wrong way of eliminating repetition below
+def do_something_with_every_item(array, operation)
+  if operation == "total" or operation == "refund"
+    amount = 0
+  end
+  
+  index = 0
+
+  while index < array.length
+    if operation == "total"
+      amount += array[index]
+    elsif operation == "refund"
+      amount -= array[index]
+    elsif operation == "show discounts"
+      amount_off = array[index] / 3.0
+      puts format("Your discount: $%.2f", amount_off)
+    end
+
+    index += 1
+  end
+
+  if operation == "total" or operation == "refund"
+    return amount
+  end
+end
+
+puts "\nWrong way of eliminating repetition. Output:"
+puts format("%.2f", do_something_with_every_item(prices, "total"))
+puts format("%.2f", do_something_with_every_item(prices, "refund"))
+puts do_something_with_every_item(prices, 'show discounts')
