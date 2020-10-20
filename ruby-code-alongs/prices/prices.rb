e077da46b69a043fb89110b70c7054f5de53e132
Cindy Lau <cindy.lau@shopify.com>
2020-10-20 14:06:55 -0400

    use .each method for the show_discounts method

--- ruby-code-alongs/prices/prices.rb
+++ ruby-code-alongs/prices/prices.rb
@@ -25,11 +25,9 @@ end
 puts format("%.2f", refund(prices))
 
 def show_discounts(prices)
-  index = 0
-  while index < prices.length
-    amount_off = prices[index] / 3.0
+  prices.each do |price|
+    amount_off = price / 3.0
     puts format("Your discount: $%.2f", amount_off)
-    index += 1
   end
 end
 
