38ba33e9375bfb671847241ceed25ed79d0eb005
Cindy Lau <cindy.lau@shopify.com>
2020-10-20 16:02:49 -0400

    use .reject to filter out reviewer bylines

--- ruby-code-alongs/reviews/reviews.rb
+++ ruby-code-alongs/reviews/reviews.rb
@@ -5,5 +5,6 @@ File.open("reviews.txt") do |review_file|
 end
 
 relevant_lines = lines.find_all { |line| line.include?("Truncated") }
+reviews = relevant_lines.reject { |line| line.include?("--") }
 
-puts relevant_lines
+puts reviews
