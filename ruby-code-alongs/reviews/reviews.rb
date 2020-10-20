690c9154710d0b5e624548a52eb45d882ba11df6
Cindy Lau <cindy.lau@shopify.com>
2020-10-20 15:47:35 -0400

    filter reviews.txt by using the ruby method .include?

--- ruby-code-alongs/reviews/reviews.rb
+++ ruby-code-alongs/reviews/reviews.rb
@@ -4,4 +4,12 @@ File.open("reviews.txt") do |review_file|
   lines = review_file.readlines
 end
 
-puts lines.length
+relevant_lines = []
+
+lines.each do |line|
+  if line.include?("Truncated")
+    relevant_lines << line
+  end
+end
+
+puts relevant_lines
