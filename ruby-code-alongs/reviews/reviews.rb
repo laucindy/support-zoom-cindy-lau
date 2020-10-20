2f9a931729b71c9c63de5a287f2c0481f9ba92ec
Cindy Lau <cindy.lau@shopify.com>
2020-10-20 15:50:12 -0400

    faster way, using .find_all, instead of .include?

--- ruby-code-alongs/reviews/reviews.rb
+++ ruby-code-alongs/reviews/reviews.rb
@@ -4,12 +4,6 @@ File.open("reviews.txt") do |review_file|
   lines = review_file.readlines
 end
 
-relevant_lines = []
-
-lines.each do |line|
-  if line.include?("Truncated")
-    relevant_lines << line
-  end
-end
+relevant_lines = lines.find_all { |line| line.include?("Truncated") }
 
 puts relevant_lines
