b23b26b76382daedeb2a6f049f36a8d8cb9d103b
Cindy Lau <cindy.lau@shopify.com>
2020-10-21 14:49:22 -0400

    add second last verse to 99 bottles song

--- Ruby/Week 3/99 Bottles/song.rb
+++ Ruby/Week 3/99 Bottles/song.rb
@@ -1,8 +1,15 @@
-def verse
-  puts "No more bottles of beer on the wall,"
-  puts "no more bottles of beer."
-  puts "Go to the store and buy some more,"
-  puts "99 bottles of beer on the wall."
+def verse(number)
+  puts "#{number} bottle of beer on the wall,"
+  puts "#{number} bottle of beer."
+  puts "Take one down and pass it around,"
+  puts "#{number} bottle of beer on the wall."
+
+  puts
+  
+  puts 'No more bottles of beer on the wall,'
+  puts 'no more bottles of beer.'
+  puts 'Go to the store and buy some more,'
+  puts '99 bottles of beer on the wall.'
 end
 
-verse
+verse(1)
