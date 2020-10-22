e33029d3649b1d0cc29241d6979367f13fd63bfe
Cindy Lau <cindy.lau@shopify.com>
2020-10-21 22:16:41 -0400

    pages 75-77, original code copy and paste for all 3 classes

---
+++ ruby-code-alongs/vehicles/vehicle.rb
@@ -0,0 +1,62 @@
+class Car
+  attr_accessor :odometer
+  attr_accessor :gas_used
+  
+  def mileage
+    @odometer / @gas_used
+  end
+
+  def accelerate
+    puts "Floor it!"
+  end
+
+  def sound_horn
+    puts "Beep! Beep!"
+  end
+
+  def steer
+    puts "Turn front 2 wheels."
+  end
+end
+
+class Motorcycle
+  attr_accessor :odometer
+  attr_accessor :gas_used
+  
+  def mileage
+    @odometer / @gas_used
+  end
+
+  def accelerate
+    puts 'Floor it!'
+  end
+
+  def sound_horn
+    puts 'Beep! Beep!'
+  end
+
+  def steer
+    puts 'Turn front 2 wheels.'
+  end
+end
+
+class Truck
+  attr_accessor :odometer
+  attr_accessor :gas_used
+  
+  def mileage
+    @odometer / @gas_used
+  end
+
+  def accelerate
+    puts 'Floor it!'
+  end
+
+  def sound_horn
+    puts 'Beep! Beep!'
+  end
+
+  def steer
+    puts 'Turn front 2 wheels.'
+  end
+end
