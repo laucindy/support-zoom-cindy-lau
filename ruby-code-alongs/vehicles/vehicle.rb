e986e863b39ad17d716b9387d78e47c1b4708e35
Cindy Lau <cindy.lau@shopify.com>
2020-10-21 22:41:24 -0400

    pages 78-80, add vehicle class

--- ruby-code-alongs/vehicles/vehicle.rb
+++ ruby-code-alongs/vehicles/vehicle.rb
@@ -1,3 +1,24 @@
+class Vehicle
+  attr_accessor :odometer
+  attr_accessor :gas_used
+
+  def mileage
+    @odometer / @gas_used
+  end
+
+  def accelerate
+    puts 'Floor it!'
+  end
+
+  def sound_horn
+    puts 'Beep! Beep!'
+  end
+
+  def steer
+    puts 'Turn front 2 wheels.'
+  end
+end
+
 class Car
   attr_accessor :odometer
   attr_accessor :gas_used
