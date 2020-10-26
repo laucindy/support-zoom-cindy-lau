d41b707797bcd30b21af08b443dfbfb3fbd5eed9
Cindy Lau <cindy.lau@shopify.com>
2020-10-26 16:25:27 -0400

    page 95, add armadillo class

--- ruby-code-alongs/animals/animal.rb
+++ ruby-code-alongs/animals/animal.rb
@@ -46,6 +46,13 @@ class Cat < Animal
   end
 end
 
+class Armadillo < Animal
+  def move(destination)
+    puts "#{@name} unrolls!"
+    puts "#{@name} runs to the #{destination}."
+  end
+end
+
 whiskers = Cat.new
 whiskers.name = "Whiskers"
 
