9c862875f75098f22872c37ec173952702afe954
Cindy Lau <cindy.lau@shopify.com>
2020-10-26 16:20:30 -0400

    pages 91-93, add animal base class, and subclasses

---
+++ ruby-code-alongs/animals/animal.rb
@@ -0,0 +1,42 @@
+class Animal
+  attr_reader :name, :age
+
+  def name=(value)
+    if value == ""
+      raise "Name can't be blank!"
+    end
+    
+    @name = value
+  end
+
+  def age=(value)
+    if value < 0
+      raise "An age of #{value} isn't valid!"
+    end
+
+    @age = value
+  end
+
+  def talk
+    puts "#{name} says Bark!"
+  end
+
+  def move(destination)
+    puts "#{@name} runs to the destination."
+  end
+
+  def report_age
+    puts "#{name} is #{@age} years old."
+  end
+
+  class Dog < Animal
+
+  end
+
+  class Bird < Animal
+
+  end
+
+  class Cat < Animal
+
+  end
