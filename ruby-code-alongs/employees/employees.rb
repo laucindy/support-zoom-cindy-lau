91ac366a186d5114a9506652ead868327ed64966
Cindy Lau <cindy.lau@shopify.com>
2020-10-26 19:57:56 -0400

    pages 147-148, inefficient factory method

--- ruby-code-alongs/employees/employees.rb
+++ ruby-code-alongs/employees/employees.rb
@@ -52,6 +52,11 @@ class HourlyEmployee < Employee
     pay_for_period = hourly_wage * hours_per_week * 2
     formatted_pay = format("$%.2f", pay_for_period)
   end
+
+  def turn_into_cashier
+    self.hourly_wage = 12.75
+    self.hours_per_week = 25
+  end
 end
 
 salaried_employee = SalariedEmployee.new("Jane Doe", 50000)
@@ -59,3 +64,17 @@ salaried_employee.print_pay_stub
 
 hourly_employee = HourlyEmployee.new("John Smith", 14.97, 30)
 hourly_employee.print_pay_stub
+
+ivan = HourlyEmployee.new("Ivan Stokes")
+ivan.turn_into_cashier
+ivan.print_pay_stub
+
+harold = HourlyEmployee.new("Harold Nguyen", 12.75, 25)
+tamara = HourlyEmployee.new("Tamara Wells", 12.75, 25)
+susie = HourlyEmployee.new("Susie Powell", 12.75, 25)
+
+edwin = HourlyEmployee.new("Edwin Burgess", 10.50, 20)
+ethel = HourlyEmployee.new("Ethel Harris", 10.50, 20)
+
+angela = HourlyEmployee.new("Angela Matthews", 19.25, 30)
+stewart = HourlyEmployee.new("Stewart Sanchez", 19.25, 30)
