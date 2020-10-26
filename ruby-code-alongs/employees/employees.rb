5972e5b99af1512229d46c60fe6a0775016fcde8
Cindy Lau <cindy.lau@shopify.com>
2020-10-26 17:36:58 -0400

    page 140, add HourlyEmployee class

--- ruby-code-alongs/employees/employees.rb
+++ ruby-code-alongs/employees/employees.rb
@@ -25,7 +25,31 @@ class SalariedEmployee < Employee
   end
 end
 
+class HourlyEmployee < Employee
+  attr_reader :hourly_wage, :hours_per_week
+
+  def hourly_wave=(hourly_wave)
+    # Code to validate and set @hours_per_week
+  end
+
+  def hours_per_week=(hours_per_week)
+    # Code to validate and set @hours_per_week
+  end
+
+  def print_pay_stub
+    print_name
+    pay_for_period = hourly_wage * hours_per_week * 2
+    formatted_pay = format("$%.2f", pay_for_period)
+  end
+end
+
 salaried_employee = SalariedEmployee.new
 salaried_employee.name = "Jane Doe"
 salaried_employee.salary = 50000
 salaried_employee.print_pay_stub
+
+hourly_employee = HourlyEmployee.new
+hourly_employee.name = "John Smith"
+hourly_employee.hourly_wage = 14.97
+hourly_employee.hours_per_week = 30
+hourly_employee.print_pay_stub
