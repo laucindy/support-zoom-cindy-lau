0cfa98da9a43cbdc523f88637a62a1f2e2bab59e
Cindy Lau <cindy.lau@shopify.com>
2020-10-27 14:31:41 -0400

    add mixin and photo class

--- ruby-code-alongs/media/media.rb
+++ ruby-code-alongs/media/media.rb
@@ -1,14 +1,22 @@
-class Clip
-  attr_reader :comments
-
-  def initialize
-    @comments = []
+module AcceptsComments
+  def comments
+    @comments ||= []
   end
 
   def add_comment(comment)
     comments << comment
   end
+end
 
+class Photo
+  include AcceptsComments
+
+  def initialize
+    @format = 'JPEG'
+  end
+end
+
+class Clip
   def play
     puts "Playing #{object_id}..."
   end
@@ -16,10 +24,12 @@ end
 
 class Video < Clip
   attr_accessor :resolution
+  include AcceptsComments
 end
 
 class Song < Clip
   attr_accessor :beats_per_minute
+  include AcceptsComments
 end
 
 video = Video.new
@@ -30,3 +40,7 @@ song = Song.new
 song.add_comment("Awesome beat.")
 
 p video.comments, song.comments
+
+photo = Photo.new
+photo.add_comment("Beautiful colors")
+p photo.comments
