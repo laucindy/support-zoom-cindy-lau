11ee068fae8558825cd04f49c3d288ec248d2ab7
Cindy Lau <cindy.lau@shopify.com>
2020-10-27 15:30:51 -0400

    add constant

--- ruby-code-alongs/comparable-and-enumerable/comparable-enumerable.rb
+++ ruby-code-alongs/comparable-and-enumerable/comparable-enumerable.rb
@@ -1,9 +1,9 @@
 class Steak
+  GRADE_SCORES = {"Prime" => 3, "Choice" => 2, "Select" => 1}
   attr_accessor :grade
 
   def >(other)
-    grade_scores = ("Prime" => 3, "Choice" => 2, "Select" => 1)
-    grade_scores(grade) > grade_scores[other.grade]
+    GRADE_SCORES[grade] > GRADE_SCORES[other.grade]
   end
 end
 
