fad48e1970818e92f62cc32c945e65840111dbc3
Cindy Lau <cindy.lau@shopify.com>
2020-10-27 15:26:46 -0400

    first attempt at a > method

---
+++ ruby-code-alongs/comparable-and-enumerable/comparable-enumerable.rb
@@ -0,0 +1,17 @@
+class Steak
+  attr_accessor :grade
+
+  def >(other)
+    grade_scores = ("Prime" => 3, "Choice" => 2, "Select" => 1)
+    grade_scores(grade) > grade_scores[other.grade]
+  end
+end
+
+first_steak = Steak.new
+first_steak.grade = "Prime"
+second_steak = Steak.new
+second_steak.grade = "Choice"
+
+if first_steak > second_steak
+  puts "I'll take #{first_steak.inspect}"
+end
