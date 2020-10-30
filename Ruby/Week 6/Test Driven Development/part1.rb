321ce0c7c58444502065ab4367c62300315d7eed
Cindy Lau <cindy.lau@shopify.com>
2020-10-30 13:33:36 -0400

    add code that passes the two tests

--- Ruby/Week 6/Test Driven Development/part1.rb
+++ Ruby/Week 6/Test Driven Development/part1.rb
@@ -1,5 +1,14 @@
 class AsciiArt
   def draw(number)
-    
+    drawing = ""
+    row = 1
+
+    while row <= number
+      row.times { drawing += "*" }
+      drawing += "\n"
+      row += 1
+    end
+
+    drawing
   end
 end
