5c3c844390e26132381e3d8d8028276dea3c482e
Cindy Lau <cindy.lau@shopify.com>
2020-10-30 15:59:50 -0400

    add code to pass tests

--- Ruby/Week 6/Test Driven Development/ascii_art.rb
+++ Ruby/Week 6/Test Driven Development/ascii_art.rb
@@ -45,6 +45,30 @@ module CenterTriangle
   end
 end
 
+module DiamondTriangle
+  def generate_triangle(number)
+    draw_half(number)
+    draw_half(number, true)
+  end
+
+  def draw_half(number, inverted = false)
+    drawing = ''
+    row = 1
+
+    while row <= number
+      space_count = inverted ? row : (number - row)
+      asterisk_count = inverted ? (number - row) : row
+
+      space_count.times { print ' ' }
+      asterisk_count.times { print '* ' }
+      print "\n"
+      row += 1
+    end
+
+    drawing
+  end
+end
+
 class AsciiArt
   def draw(number)
     drawing = generate_triangle(number)
@@ -52,5 +76,5 @@ class AsciiArt
 end
 
 #ascii_art = AsciiArt.new { include LeftTriangle }
-#ascii_art = AsciiArt.new.extend CenterTriangle
+#ascii_art = AsciiArt.new.extend DiamondTriangle
 #puts ascii_art.draw(3)
