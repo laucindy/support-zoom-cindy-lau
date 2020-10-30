62ed96e794943936bbef52d711dd74b9a902c956
Cindy Lau <cindy.lau@shopify.com>
2020-10-30 15:59:28 -0400

    add test code for diamond shape

--- Ruby/Week 6/Test Driven Development/tests.rb
+++ Ruby/Week 6/Test Driven Development/tests.rb
@@ -50,4 +50,19 @@ class AsciiArtTest < Minitest::Test
     expected = "\n"
     assert_output(expected) { puts ascii_art.draw(0) }
   end
+
+  def test_outputs_triangle_of_asterisks_diamond_shape
+    ascii_art = AsciiArt.new.extend DiamondTriangle
+    expected_3_stars = "  * \n * * \n* * * \n * * \n  * \n   \n\n"
+    expected_4_stars = "   * \n  * * \n * * * \n* * * * \n * * * \n  * * \n   * \n    \n\n"
+
+    assert_output(expected_3_stars) { puts ascii_art.draw(3) }
+    assert_output(expected_4_stars) { puts ascii_art.draw(4) }
+  end
+
+  def test_empty_output_triangle_of_asterisks_diamond_shape
+    ascii_art = AsciiArt.new.extend DiamondTriangle
+    expected = "\n"
+    assert_output(expected) { puts ascii_art.draw(0) }
+  end
 end
