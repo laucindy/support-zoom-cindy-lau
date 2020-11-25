851a1987566f154632c2c690f2ecf27b1669b7d1
Cindy Lau <cindy.lau@shopify.com>
2020-11-25 11:26:07 -0500

    initial solution

--- Dev Problems/Gilded Rose/gilded_rose.rb
+++ Dev Problems/Gilded Rose/gilded_rose.rb
@@ -5,49 +5,41 @@ class GildedRose
   end
 
   def update_quality()
+    aged_brie = "Aged Brie"
+    backstage_passes = "Backstage passes to a TAFKAL80ETC concert"
+    conjured_mana_cake = 'Conjured Mana Cake'
+    sulfuras = 'Sulfuras, Hand of Ragnaros'
+    max_quality = 50
+
+    items_that_increase_in_quality = [aged_brie, backstage_passes]
+    legendary_items = [sulfuras]
+    
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
+      case item.name
+      when aged_brie
+        item.quality += 1
+        item.quality += 1 if (item.sell_in <= 0) && (item.quality < max_quality)
+        item.sell_in -= 1
+      when backstage_passes
+        if item.sell_in > 0
+          item.quality += 1
+          item.quality += 1 if (item.sell_in <= 10) && (item.quality < max_quality)
+          item.quality += 1 if (item.sell_in <= 5) && (item.quality < max_quality)
+          
+          item.sell_in -= 1
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality = 0
+          item.sell_in -= 1
         end
+      when conjured_mana_cake
+        quality_degradation = (item.sell_in > 0) ? 2 : 4
+        item.quality -= quality_degradation if item.quality < max_quality
+        item.sell_in -= 1
+      when sulfuras
+        item.quality = 80
+      else
+        item.quality -= 1 if item.quality < max_quality
+        item.sell_in -= 1
       end
     end
   end
--- Dev Problems/Gilded Rose/texttest_fixture.rb
+++ Dev Problems/Gilded Rose/texttest_fixture.rb
@@ -10,8 +10,8 @@ items = [
   Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
   Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=45),
+  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=48),
   # This Conjured item does not work properly yet
   Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
 ]
